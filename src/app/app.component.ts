462c0ac2b10d528bccfa303c541168536331fcb7
John Lee <jlee@openlane.com>
2017-03-21 09:21:11 -0700

    templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  	<h1>Inline Template</h1>
+  `,
+  styles: [`
+  	h1 {
+  		color: red;
+  	}
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular 2 App!';
 }
